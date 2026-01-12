39f66401532cbc0ea258122b75851d67a2e4b04f
ghimire-sunil <sunil.ghimire@smarten.com.np>
2026-01-12 15:03:45 +0545

    ..

--- st_general_ledger/models/partner_ledger.py
+++ st_general_ledger/models/partner_ledger.py
@@ -150,3 +150,23 @@ class PartnerLedgerCustomHandler(models.AbstractModel):
                     break
 
         return line_data
+class AccountMoveInherited(models.Model):
+    _inherit = "account.move"
+
+    move_type = fields.Selection(
+        selection=[
+            ('entry', 'Journal Entry'),
+            ('out_invoice', 'Customer Invoice'),
+            ('out_refund', 'Customer Credit Note'),
+            ('in_invoice', 'Vendor Bill'),
+            ('in_refund', 'Vendor Debit Note'),
+            ('out_receipt', 'Sales Receipt'),
+            ('in_receipt', 'Purchase Receipt'),
+        ],
+        string='Type',
+        required=True,
+        tracking=True,
+        change_default=True,
+        index=True,
+        default="entry",
+    )
